9ff114971fe24b1d03f036a93f508d1fe1a69f1a
Changhyeon Lee <changhyl@student.42seoul.kr>
2023-10-03 21:41:46 +0900

    1

---
+++ cpp_Module02/ex01/Fixed.hpp
@@ -0,0 +1,22 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+	private:
+		int	fp_value;
+		static const int	frac_bits = 8;
+	public:
+		Fixed();
+		Fixed(const int raw);
+		Fixed(const float raw);
+		Fixed(const Fixed	&fixed);
+		Fixed& operator=(const Fixed& fixed);
+		~Fixed();
+		int	getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int	toInt(void) const;
+};
+
+#endif
