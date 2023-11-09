fe5465f2db622b13e51c80037b46630a6c27b2f2
Changhyeon Lee <changhyl@student.42seoul.kr>
2023-11-09 18:24:48 +0900

    1

--- cpp_Module01/ex03/HumanA.cpp
+++ cpp_Module01/ex03/HumanA.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string name, Weapon weapon)
+HumanA::HumanA(std::string name, Weapon& weapon) : weapon(weapon)
 {
 	this->name = name;
-	this->weapon = weapon;
 }
 
 void	HumanA::attack(void)
--- cpp_Module01/ex03/HumanB.cpp
+++ cpp_Module01/ex03/HumanB.cpp
@@ -4,15 +4,16 @@
 HumanB::HumanB(std::string name)
 {
 	this->name = name;
+	this->weapon = NULL;
 }
 
-void	HumanB::setWeapon(Weapon weapon)
+void	HumanB::setWeapon(Weapon& weapon)
 {
-	this->weapon = weapon;
+	this->weapon = &weapon;
 }
 
 void	HumanB::attack(void)
 {
-	std::cout << this->name << " attacks their " << this->weapon.getType()
+	std::cout << this->name << " attacks their " << this->weapon->getType()
 		<< std::endl;
 }
