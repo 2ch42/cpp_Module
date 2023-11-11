5d774ebccf1d39f4a5306bec55f4ff58decb241a
Changhyeon Lee <changhyl@student.42seoul.kr>
2023-11-11 20:30:48 +0900

    1

--- cpp_Module01/ex06/main.cpp
+++ cpp_Module01/ex06/main.cpp
@@ -3,16 +3,15 @@
 int	arg_to_num(char *argv)
 {
 	std::string s(argv);
-	if (s == "DEBUG")
-		return (1);
-	else if (s == "INFO")
-		return (2);
-	else if (s == "WARNING")
-		return (3);
-	else if (s == "ERROR")
-		return (4);
-	else
-		return (0);
+	std::string str_arr[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i = 0; i < 4; i++)
+	{
+		if (s == str_arr[i])
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
 
 int main(int argc, char *argv[])
@@ -26,19 +25,19 @@ int main(int argc, char *argv[])
 
 	switch (arg_to_num(argv[1]))
 	{
-		case 1:
+		case 0:
 			std::cout << "[ DEBUG ]" << std::endl;
 			harl.complain("DEBUG");
 			std::cout << "\n";
-		case 2:
+		case 1:
 			std::cout << "[ INFO ]" << std::endl;
 			harl.complain("INFO");
 			std::cout << "\n";
-		case 3:
+		case 2:
 			std::cout << "[ WARNING ]" << std::endl;
 			harl.complain("WARNING");
 			std::cout << "\n";
-		case 4:
+		case 3:
 			std::cout << "[ ERROR ]" << std :: endl;
 			harl.complain("ERROR");
 			std::cout << "\n";
