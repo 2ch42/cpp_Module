af74a7ea50a9b35d3ca5d2c5cf4b2f6bdf1f2d20
Changhyeon Lee <changhyl@student.42seoul.kr>
2023-12-21 20:58:11 +0900

    1

---
+++ cpp_Module06/ex02/Base.cpp
@@ -0,0 +1,46 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+Base::~Base() {}
+
+Base *generate(void)
+{
+    srand(time(NULL)); // should use another function
+    if (rand() % 3 == 1)
+    {
+        Base *base = new A();
+        return base;
+    }
+    else if (rand() % 3 == 2)
+    {
+        Base *base = new B();
+        return base;
+    }
+    else
+    {
+        Base *base = new C();
+        return base;
+    }
+}
+
+void    identify(Base *p)
+{
+    if (dynamic_cast<A *>(p))
+        std::cout << "Actual type : A" << std::endl;
+    else if (dynamic_cast<B *>(p))
+        std::cout << "Actual type : B" << std::endl;
+    else
+        std::cout << "Actual type : C" << std::endl;
+}
+
+/*void    identify(Base& p)
+{
+
+}
+
+*/
---
+++ cpp_Module06/ex02/main.cpp
@@ -0,0 +1,25 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+
+int main()
+{
+    Base *test1 = generate();
+    Base *test2 = generate();
+    Base *test3 = generate();
+
+    std::cout << "Test1 : ";
+    identify(test1);
+    std::cout << "Test2 : ";
+    identify(test2);
+    std::cout << "Test3 : ";
+    identify(test3);
+
+    delete test1;
+    delete test2;
+    delete test3;
+
+    return (0);
+}
