47d96b37aed2d7685d8ba9de6a1647a56fde977e
Changhyeon Lee <changhyl@student.42seoul.kr>
2024-01-05 14:05:44 +0900

    1

--- cpp_Module08/ex00/easyfind.hpp
+++ cpp_Module08/ex00/easyfind.hpp
@@ -1,8 +1,24 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <iostream>
 # include <vector>
+# include <list>
+# include <deque>
 # include <stdexcept>
 
+template <typename T>
+typename T::iterator easyfind(T& container, int key)
+{
+    for(typename T::iterator it = container.begin(); it != container.end(); it++)
+    {
+        if (*it == key)
+        {
+            std::cout << "KEY FOUND : " << *it << std::endl;
+            return (it);
+        }
+    }
+    throw (std::runtime_error("Given key doesn't exist!"));
+}
 
 #endif
--- cpp_Module08/ex00/main.cpp
+++ cpp_Module08/ex00/main.cpp
@@ -1,7 +1,59 @@
-#include <iostream>
 #include "easyfind.hpp"
 
 int main()
 {
-    
+    try
+    {
+        std::cout << "-----Testing vector..-----" << std::endl;
+        std::vector<int> int_vec;
+        int_vec.push_back(10);
+        int_vec.push_back(20);
+        int_vec.push_back(30);
+        int_vec.push_back(40);
+        int_vec.push_back(50);
+        std::vector<int>::iterator it = easyfind(int_vec, 20);
+        it = easyfind(int_vec, 70);
+        it = easyfind(int_vec, 40);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        std::cout << "-----Testing deque..-----" << std::endl;
+        std::deque<int> int_list;
+        int_list.push_back(10);
+        int_list.push_back(20);
+        int_list.push_back(30);
+        int_list.push_back(40);
+        int_list.push_back(50);
+        std::deque<int>::iterator it = easyfind(int_list, 20);
+        it = easyfind(int_list, 70);
+        it = easyfind(int_list, 40);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        std::cout << "-----Testing deque..-----" << std::endl;
+        std::deque<int> int_deque;
+        int_deque.push_back(10);
+        int_deque.push_back(20);
+        int_deque.push_back(30);
+        int_deque.push_back(40);
+        int_deque.push_back(50);
+        std::deque<int>::iterator it = easyfind(int_deque, 20);
+        it = easyfind(int_deque, 70);
+        it = easyfind(int_deque, 40);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return (0);
 }
