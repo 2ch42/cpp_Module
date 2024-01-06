db39af5a2102a6cd3a885ce71488a5358f7f000a
Changhyeon Lee <changhyl@student.42seoul.kr>
2024-01-06 20:57:09 +0900

    1

--- cpp_Module08/ex02/MutantStack.hpp
+++ cpp_Module08/ex02/MutantStack.hpp
@@ -1,11 +1,29 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-class MutantStack
-{
-    private:
-        
+# include <stack>
+# include <vector>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{  
     public:
+        MutantStack() {}
+        MutantStack(const MutantStack& mutantstack)
+        {
+            *this = mutantstack;
+        }
+        MutantStack& operator=(const MutantStack& mutantstack)
+        {
+            if (this != mutantstack)
+                *this = mutantstack;
+            return (*this);
+        }
+        ~MutantStack() {}
+        typedef typename MutantStack<T>::stack::container_type::iterator iterator;
+        iterator    begin() { return (this->c.begin()); }
+        iterator    end() { return (this->c.end()); }
+
 };
 
 #endif
