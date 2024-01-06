60697f27bcc251ce872934b9312e054b0931d7bc
Changhyeon Lee <changhyl@student.42seoul.kr>
2024-01-06 18:41:32 +0900

    1

--- cpp_Module08/ex01/main.cpp
+++ cpp_Module08/ex01/main.cpp
@@ -36,19 +36,14 @@ int main()
     try
     {
         std::cout << "----- Test 3 -----" << std::endl;
-        Span span(10);
-        span.addNumber(1);
-        span.addNumber(9);
-        span.addNumber(8);
-        span.addNumber(3);
-        span.addNumber(7);
-        span.addNumber(5);
-        span.addNumber(4);
-        span.addNumber(6);
-        span.addNumber(2);
-        span.addNumber(0);
-        std::cout << "Shortest Span... " << span.shortestSpan() << std::endl; //
-        std::cout << "Longest Span... " << span.longestSpan() <<std::endl;
+        Span sp = Span(5);
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
     }
     catch(const std::exception& e)
     {
